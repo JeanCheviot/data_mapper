70fd219eef39bc3cc23a43e293c422ead4460ae5
Joe Vincent <jeancheviot@icloud.com>
2016-10-20 14:51:47 +0100

    Goddammit Mary

--- models/tag.rb
+++ models/tag.rb
@@ -11,3 +11,7 @@ class Tag
   property :label, String
 
 end
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
